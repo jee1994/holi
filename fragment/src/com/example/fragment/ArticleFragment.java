ed0b1ae6824c913b2ab1c4ae3584ce1903166899
jee1994 <gkswldns10@gmail.com>
2014-10-18 15:17:49 -0500

    fdjnvk

--- fragment/src/com/example/fragment/ArticleFragment.java
+++ fragment/src/com/example/fragment/ArticleFragment.java
@@ -1,13 +1,5 @@
 package com.example.fragment;
 
-import com.example.fragment.R;
-import com.example.fragment.R.layout;
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class ArticleFragment extends Fragment {
     @Override
